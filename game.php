66888b941711828d65afc3900a6477e67b484b73
Eddy Mac <eddymac11@outlook.com>
2020-10-18 00:54:16 +0300

    Other changes

--- game.php
+++ game.php
@@ -1,5 +1,39 @@
 <?php
-if (! isset($_GET['name']) || strlen(($_GET['name']) < 1)) {
+if (! isset($_GET['name'])) {
   die("Missing name parameter. You have no permission to play!");
 }
+
+if (isset($_POST['lgt'])) {
+  header("Location: login.php");
+  return;
+}
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>RPS - Game</title>
+  <?php require_once("bootstrap.php");?>
+</head>
+<body>
+  <div class="container">
+  <h1><?php echo "Hi ".$_GET['name'].",\n"?>Thank You and welcome</h1>
+  <p>Here, you will select an option among rock, paper, and scissors.
+  Let's see how you do against the computer!</p>
+  <form method="post">
+  <select name="" id="">
+  <option value="0">--Please select an option--</option>
+  <option value="1">Rock</option>
+  <option value="2">Paper</option>
+  <option value="3">Scissors</option>
+  </select>
+  <input type="submit" value="Play">
+  <input type="submit" name="lgt" value="Logout">
+  </form>
+  </div>
+</body>
+</html>
+
--- login.php
+++ login.php
@@ -1,3 +1,30 @@
+<?php
+if (isset($_POST['cancel'])) {
+  header("Location: index.php");
+  return;
+}
+
+$message = false;
+
+$salt = 'XyZzy12*_';
+$stored_hash = '1a52e17fa899cf40fb04cfc42e6352f1';
+
+
+if (isset($_POST['user']) && isset($_POST['pass'])) {
+  if (strlen($_POST['user']) < 1 || strlen($_POST['pass']) < 1) {
+    $message = "Username and password required";
+  } else {
+    $check = hash('md5', $salt.$_POST['pass']);
+    if ($check == $stored_hash) {
+      header("Location: game.php?name=".urlencode($_POST['user']));
+      return;
+    } else {
+      $message = "Incorrect Password";
+    }
+  }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,7 +38,7 @@
  <h1>Please log in</h1>
 
  <?php
- 
+ echo('<p style="color:red;">'.htmlentities($message)."</p>");
  ?>
 
  <form method="post">
